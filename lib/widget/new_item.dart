0bc2bec5b2708c51933ff521e998736f289de136
NavidZamanKhan <navid166418@gmail.com>
2025-05-28 00:33:33 +0600

    added real time database

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
-import 'package:shopping_list_app/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,14 +23,27 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _title,
-          quantity: _enteredAmount,
-          category: _selecedCategory,
-        ),
+      final url = Uri.https(
+        "flutter-prep-546fc-default-rtdb.firebaseio.com",
+        "shopping_list.json",
       );
+      http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({
+          "name": _title,
+          "quantity": _enteredAmount,
+          "category": _selecedCategory.title,
+        }),
+      );
+      // Navigator.of(context).pop(
+      //   GroceryItem(
+      //     id: DateTime.now().toString(),
+      //     name: _title,
+      //     quantity: _enteredAmount,
+      //     category: _selecedCategory,
+      //   ),
+      // );
     }
   }
 
